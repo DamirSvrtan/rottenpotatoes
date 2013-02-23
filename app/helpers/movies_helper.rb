5f1b57eb17d47799836f62fdd712ab62be624f48
DamirSvrtan <damir.svrtan@gmail.com>
2013-02-23 21:53:44 +0100

    My initial commit

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,17 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def sortable(column)   
+   return link_to "Movie title", {:sort => column}, {:class => 'title_header'} if column == 'title'
+   return link_to column.titleize, {:sort => column} , {:class => 'release_date_header'}
+  end
+ 
+  def hilite(nesto)
+	if @hilite == nesto
+	return 'hilite'
+	else
+	return 'ne-hilite'
+	end
+  end
 end
